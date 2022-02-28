4a3e74aaef96f41da1b429e209e72339e69c2b8a
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-02-28 22:04:09 +0300

    Code Challenge 01 - Reverse Array

---
+++ array_reverse/array_reverse.py
@@ -0,0 +1,21 @@
+def reverse_array(arr):
+    """
+    Function that reverse the elements of a list
+
+    Input: List 
+
+    Output: Returns list of reversed elements
+
+    """
+
+    arr_reversed = [None] * len(arr)
+    start_index = 0
+    index = len(arr)-1
+    while index > -1:
+        arr_reversed[start_index] = arr[index]
+        index -= 1
+        start_index += 1
+    return arr_reversed
+
+reverse_array(arr)
+

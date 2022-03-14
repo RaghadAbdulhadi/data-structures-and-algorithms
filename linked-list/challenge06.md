73b30627bf9e003e2595d90f8a079be2fc579eac
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-03-14 23:35:57 +0300

    Linked List Insertions

---
+++ linked-list/challenge06.md
@@ -0,0 +1,77 @@
+# Challenge Summary
+Create a Linked list with append, insert_after, insert_before methods.
+All methods will be implemented inside the Linked list Class, and intializing the nodes will be in a seperated Node class.
+**Four Methods will be added:**
+
+**Append Method:**
+A function that adds a new node with the value to the the end of the list.
+Arguments: value
+Returns: The linked list with the new node at the end of the linked list
+
+**Insert After:**
+A function that adds a new node with the value after the given value of the node in the list.
+Arguments: value, new_value
+Returns: The linked list with the new node added after the given value of existing node
+
+**Insert Before:**
+A function that adds a new node with the value before the given value of the node in the list.
+Arguments: value, new_value
+Returns: The linked list with the new node added before the given value of existing node
+
+**Remove Node:**
+A function that removes node with a specific index(first, second, third node) from the list
+Input: index of the node
+Output: L.L with the node removed
+
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+**Append Method:**
+
+![Append Whiteboard Workflow](./images/AppendWB.PNG)
+
+**Insert After Method:**
+
+![Insert After Whiteboard Workflow](./images/InsertAfterWB.PNG)
+
+**Insert Before Method:**
+
+![Insert Before Whiteboard Workflow](./images/InsertBeforeWB.PNG)
+
+
+## Approach & Efficiency
+
+**Append Node:**
+1. Create an append method that takes a value as an argument
+2. Check if the linked list is empty(If self.head in None)
+3. Reassign self.head to be the node instantiated from the node class
+4. Check if the linked list is not empty
+5. Define a variable to be the self.head (the first node in the linked list)
+6. Loop over the linked list until we reach the node pointing to None (the last node in the linked list)
+7. Assign the next of the last node to be the new object that we will instatiate from the Class node, with the value equal to the argument passed, and the next value pointing to None.
+
+**Insert After:**
+1. Create an insert_after method that takes a value and a new value to be added as arguments
+2. Check if the linked list is empty(If self.head in None)
+3. Reassign self.head to be the node instantiated from the node class
+4. Check if the linked list is not empty
+5. Define a variable to be the self.head (the first node in the linked list)
+6. Loop over the linked list until we reach the node that has a value equal to the value passed in the function
+7. Instantiate a node from the node class with a value of the new_value passed to the function, and the next pointing to the current node that we reached next
+8. Assign the current node to be pointing to the new added node
+
+**Insert Before:**
+1. Create an insert_before method that takes a value and a new value to be added as arguments
+2. Check if the linked list is empty(If self.head in None)
+3. Reassign self.head to be the node instantiated from the node class
+4. Check if the linked list is not empty
+5. Define a variable to be the self.head (the first node in the linked list)
+6. Loop over the linked list until we reach the node that has the next value pointing to the value passed in the function
+7. Instantiate a node from the node class with a value of the new_value passed to the function, and the next pointing to the current node that we reached next
+8. Assign the current node to be pointing to the new added node
+**Big O:**
+- Insert/Delete Element at the end = O(n)
+- Insert Element at the middle = O(n)
+
+## Solution
+![Solution](./images/solution%231.PNG)
--- linked-list/linked_list/linked_list.py
+++ linked-list/linked_list/linked_list.py
@@ -14,6 +14,7 @@ class linked_list:
     """
     A class that instantiate, insert at the begining, insert at the end, removes nodes from a linked list and get the length of the list and checks if it contains a specific value.
     """
+
     def __init__(self):
         """
         When instantiation an empty linked list will be created.
@@ -21,16 +22,21 @@ class linked_list:
         #Points to the head of the linked list
         self.head = None
 
-    def insert(self, value):
+    def insert_at_beginning(self, value):
         """
         A function that adds a new node with the value to the the begining of the list, with O(1) time performance.
             Arguments: value
-            Returns: Nothing
+            Returns: The linked list with the new node added at the begining of the list
         """
         node = Node(value, self.head)
         self.head = node
 
-    def insert_at_end(self, value):
+    def append(self, value):
+        """
+        A function that adds a new node with the value to the the end of the list.
+            Arguments: value
+            Returns: The linked list with the new node at the end of the linked list
+        """
         if self.head is None:
             self.head = Node(value, None)
             return
@@ -40,9 +46,43 @@ class linked_list:
                 current = current.next
             else: current.next = Node(value, None)
 
+    def insert_after(self, value, new_value):
+        """
+        A function that adds a new node with the value after the given value of the node in the list.
+            Arguments: value, new_value
+            Returns: The linked list with the new node added after the given value of existing node
+        """
+        if self.head is None:
+            self.head = Node(new_value, None)
+        else:
+            current = self.head
+            while current:
+                if current.value == value:
+                    node = Node(new_value, current.next)
+                    current.next = node
+                    break
+                current = current.next
+
+    def insert_before(self, value, new_value):
+        """
+        A function that adds a new node with the value before the given value of the node in the list.
+            Arguments: value, new_value
+            Returns: The linked list with the new node added before the given value of existing node
+        """
+        if self.head is None:
+            self.head = Node(new_value, None)
+        else:
+            current = self.head
+            while current:
+                if current.next.value == value:
+                    node = Node(new_value, current.next)
+                    current.next = node
+                    break
+                current = current.next
+
     def insert_values(self, values_list):
         for value in values_list:
-            self.insert_at_end(value)
+            self.append(value)
 
     def get_length(self):
         count = 0
@@ -53,6 +93,11 @@ class linked_list:
         return count
     
     def remove_at(self, index):
+        """
+        A function that removes node with a specific index(first, second, third node) from the list
+        Input: index of the node
+        Output: L.L with the node removed
+        """
         if index < 0 or index >= self.get_length():
             raise Exception("Invalid Index")
 
@@ -128,21 +173,20 @@ if __name__ == '__main__':
 
     ll = linked_list()
     ll2 = linked_list()
-
-    ll.insert("Raghad")
-    ll.insert("Gheed")
-    ll.insert_at_end("Jood")
+    ll.insert_at_beginning("Raghad")
+    ll.insert_at_beginning("Gheed")
+    ll.append("Jood")
+    ll.insert_after("Raghad", "Omar")
+    ll.insert_before("Raghad", "mugh")
+    # ll.insert_before("Gheed", "moayad")
     ll.insert_values(["Omar", "Ayman", "Rajaa"])
     ll.remove_at(1)
-    ll.remove_at(20)
-    ll.insert_at()
-    ll.insert_at(2,"Jood")
-    ll.insert_at(0,"mugh")
-
-    ll2.insert_values(["sewar", "rand", "raghad"])
-
+    # ll.remove_at(20)
+    # ll.insert_at()
+    # ll.insert_at(2,"Jood")
+    # ll.insert_at(0,"mugh")
+    # ll2.insert_values(["sewar", "rand", "raghad"])
     print(ll.includes("Rand"))
     print(ll.to_string())
     print(ll.get_length())
-
     print(ll2.to_string())
